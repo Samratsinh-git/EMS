6b1acb4f9d797816762c4e5a7cb07bc0bf1bd459
Rushi_0508 <rushigandhi14@gmail.com>
2024-02-24 12:19:40 +0530

    Login Func

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import AuthContext from './context/AuthContext'
 
 const poppins = Poppins({ subsets: ['latin'], weight: "400", display: "swap" })
 
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <AuthContext>
           {children}
+        </AuthContext>
       </body>
     </html>
   )
